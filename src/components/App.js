7fe98a76d7155038fc46a94e61191ea755953358
megha <meghadongre6@gmail.com>
2023-07-16 20:01:09 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,18 @@ import '../styles/App.css';
 
 const App = () => {
 //code here 
+const[statusColor,setColor] = useState("redColor");
+const clicked =()=>{
+  if(statusColor === "redColor"){
+    setColor("blueColor");
+  }else{
+    setColor("redColor");
+  }
+};
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={statusColor} >Newton School</p>
+      <button id='button' onClick={clicked}>Change Style</button>
     </div>
   )
 }
